7c1974bd816187aac132fccbe2cc6237c2d47c0d
lucasj9 <72234709+lucasj9@users.noreply.github.com>
2021-02-25 10:23:16 -0800

    Added DB Connection
    First step, need to add DB dump will connect to individual DB and test soon

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = 'classmysql.engr.oregonstate.edu';
+$dbuser = 'cs361_username';
+$dbpass = 'password';
+$dbname = 'cs361_username'; 
+
+// connect to database with parameters from above
+$db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+?>
