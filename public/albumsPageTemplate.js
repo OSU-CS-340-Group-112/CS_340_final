f7113f4db0bb97bee03c24bbaa9eeb0cc5a736d7
Sean-Michael <seanm.riesterer@gmail.com>
2021-03-14 22:09:00 -0700

    Trying to get Album-Artist relationship :/

--- public/albumsPageTemplate.js
+++ public/albumsPageTemplate.js
@@ -19,8 +19,21 @@ templates['albumsPage'] = template({"1":function(container,depth0,helpers,partia
     + ")\">Delete</button></td>\n                <td><a href=\"/albums/"
     + alias4(((helper = (helper = lookupProperty(helpers,"albumID") || (depth0 != null ? lookupProperty(depth0,"albumID") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"albumID","hash":{},"data":data,"loc":{"start":{"line":24,"column":37},"end":{"line":24,"column":48}}}) : helper)))
     + "\">Update</a></td>\n            </tr>\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "        <option value=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"artistID") || (depth0 != null ? lookupProperty(depth0,"artistID") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"artistID","hash":{},"data":data,"loc":{"start":{"line":34,"column":23},"end":{"line":34,"column":35}}}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"primaryArtist") || (depth0 != null ? lookupProperty(depth0,"primaryArtist") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"primaryArtist","hash":{},"data":data,"loc":{"start":{"line":34,"column":37},"end":{"line":34,"column":54}}}) : helper)))
+    + "</option>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
         }
@@ -28,7 +41,9 @@ templates['albumsPage'] = template({"1":function(container,depth0,helpers,partia
     };
 
   return "<script src=\"deletealbum.js\"></script>\n\n\n<div class=\"container\">\n    <center>\n    <div id=\"landing-pg\">\n    <h1>Albums</h1>\n    <h3>This page allows access to the Albums in the database.</h3>\n    </center>\n</div>\n    <table>\n        <thead>\n            <th>AlbumID</th>\n            <th>Featued Artist</th>\n            <th>Album Title</th>\n        </thead>\n        <tbody>\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"album") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":18,"column":12},"end":{"line":26,"column":21}}})) != null ? stack1 : "")
-    + "        </tbody>\n    </table>\n\n    <form id=\"addalbum\" action=\"/albums\" method=\"post\">\n    Featured Artist: <input type=\"text\" name=\"featuredArtist\"><br>\n    Album Title: <input type=\"text\" name=\"albumTitle\"><br>\n    <input type=\"submit\" value=\"Submit\">\n    </form>\n";
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"album") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":18,"column":12},"end":{"line":26,"column":21}}})) != null ? stack1 : "")
+    + "        </tbody>\n    </table>\n\n    <form id=\"addalbum\" action=\"/albums\" method=\"post\">\n    Album Title: <input type=\"text\" name=\"albumTitle\"><br>\n    Artist: <select name=\"primaryArtist\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"artists") : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":33,"column":8},"end":{"line":35,"column":17}}})) != null ? stack1 : "")
+    + "    </select>\n    <input type=\"submit\" value=\"Submit\">\n    </form>\n";
 },"useData":true});
 })();
--- public/update-AlbumTemplate.js
+++ public/update-AlbumTemplate.js
@@ -1,14 +1,27 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['update-album'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, alias1=container.lambda, alias2=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+templates['update-album'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
         }
         return undefined
     };
 
-  return "<script>\n    function updateAlbum(albumID){\n    $.ajax({\n        url: '/albums/' + albumID,\n        type: 'PUT',\n        data: $('#update-album').serialize(),\n        success: function(result){\n            window.location.replace(\"./\");\n        }\n    })\n};\n\n</script>\n\n<h1>Update "
+  return "        <option value=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"artistID") || (depth0 != null ? lookupProperty(depth0,"artistID") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"artistID","hash":{},"data":data,"loc":{"start":{"line":25,"column":23},"end":{"line":25,"column":35}}}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"primaryArtist") || (depth0 != null ? lookupProperty(depth0,"primaryArtist") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"primaryArtist","hash":{},"data":data,"loc":{"start":{"line":25,"column":37},"end":{"line":25,"column":54}}}) : helper)))
+    + "</option>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<script>\n    function updateAlbum(albumID){\n        $.ajax({\n            url: '/albums/' + albumID,\n            type: 'PUT',\n            data: $('#update-album').serialize(),\n            success: function(result){\n                window.location.replace(\"./\");\n            }\n        })\n    };\n\n    function selectArtist(artistID){\n        $(\"#artist-selector\").val(artistID);\n    }\n\n</script>\n\n<h1>Update "
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"album") : depth0)) != null ? lookupProperty(stack1,"featuredArtist") : stack1), depth0))
     + " "
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"album") : depth0)) != null ? lookupProperty(stack1,"albumTitle") : stack1), depth0))
@@ -16,7 +29,11 @@ templates['update-album'] = template({"compiler":[8,">= 4.3.0"],"main":function(
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"album") : depth0)) != null ? lookupProperty(stack1,"featuredArtist") : stack1), depth0))
     + "\"><br>\n    Album Title: <input type=\"text\" name=\"albumTitle\" value=\""
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"album") : depth0)) != null ? lookupProperty(stack1,"albumTitle") : stack1), depth0))
-    + "\"><br>\n</form>\n<button id=\"update\"onclick=\"updateAlbum("
+    + "\"><br>\n    Featured Artist: <select name=\"featuredArtist\" id=\"artist-selector\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias3,(depth0 != null ? lookupProperty(depth0,"artists") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":24,"column":8},"end":{"line":26,"column":17}}})) != null ? stack1 : "")
+    + "    </select>\n</form>\n<button id=\"update\"onclick=\"selectArtist("
+    + alias2(((helper = (helper = lookupProperty(helpers,"artistID") || (depth0 != null ? lookupProperty(depth0,"artistID") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias3,{"name":"artistID","hash":{},"data":data,"loc":{"start":{"line":29,"column":41},"end":{"line":29,"column":53}}}) : helper)))
+    + ") updateAlbum("
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"album") : depth0)) != null ? lookupProperty(stack1,"albumID") : stack1), depth0))
     + ")\">Update</button>";
 },"useData":true});
