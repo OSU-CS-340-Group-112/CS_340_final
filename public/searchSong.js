ba949f0717d40f7a01b4d30b5c66810fa90c2d82
lucasj9 <72234709+lucasj9@users.noreply.github.com>
2021-03-15 20:53:09 -0700

    song change #2

--- public/searchSong.js
+++ public/searchSong.js
@@ -1,4 +1,4 @@
 function searchSongbyTitle() {
     var song_string = document.getElementById('song_title_search').value
-    window.location = '/song/search/' + encodeURI(song_title_search)
+    window.location = '/songs/search/' + encodeURI(song_title_search)
 }
